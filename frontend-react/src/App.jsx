eb5db2cfa4f8d7d76926217ffc67c7f788199561
lucianolivares <lucianolivares@gmail.com>
2022-02-20 14:43:12 -0300

    lazy import App and home

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,14 +1,16 @@
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './globalStyles'
-import Home from './pages/Home'
-import RestaurantsPage from './pages/RestaurantsPage'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Menu from './pages/Menu'
-import SignUp from './pages/SignUp'
-import AboutUs from './pages/AboutUsPage'
-import CategoriesPage from './pages/CategoriesPage'
+
+const Home = lazy(() => import('./pages/Home'))
+const Menu = lazy(() => import('./pages/Menu'))
+const RestaurantsPage = lazy(() => import('./pages/RestaurantsPage'))
+const SignUp = lazy(() => import('./pages/SignUp'))
+const AboutUs = lazy(() => import('./pages/AboutUsPage'))
+const CategoriesPage = lazy(() => import('./pages/CategoriesPage'))
 // import ScrollToTop from './pages/ScrollToTop';
 
 const theme = {
@@ -28,20 +30,23 @@ function App () {
   return (
     <ThemeProvider theme={theme}>
       <Router>
+
         <GlobalStyle />
         {/* <ScrollToTop /> */}
 
         <Navbar />
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='restaurants' element={<RestaurantsPage />} />
-          <Route path='categories' element={<CategoriesPage />} />
-          <Route path='restaurant-view/:id' element={<Menu />} />
-          <Route path='sign-up' element={<SignUp />} />
-          <Route path='about-us' element={<AboutUs />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='restaurants' element={<RestaurantsPage />} />
+            <Route path='categories' element={<CategoriesPage />} />
+            <Route path='restaurant-view/:id' element={<Menu />} />
+            <Route path='sign-up' element={<SignUp />} />
+            <Route path='about-us' element={<AboutUs />} />
+          </Routes>
 
+        </Suspense>
         <Footer />
       </Router>
     </ThemeProvider>
